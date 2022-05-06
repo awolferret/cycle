c97d97cf57b38fd435f24e33bcdf2801d1326ac0
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-06 20:30:50 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите ваше сообщение");
+            string massage = Console.ReadLine();
+            Console.WriteLine("Сколько повторов?");
+            int repeatNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < repeatNumber; i++)
+            {
+                Console.WriteLine(massage);
+            }
+
+        }
+    }
+}
